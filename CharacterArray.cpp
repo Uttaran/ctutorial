9bb169f4a222df435921aa4cc3e072af61620d3c
Uttaran <uttarantutorial@gmail.com>
2015-06-15 22:55:11 +0200

    adding corrected file

--- CharacterArray.cpp
+++ CharacterArray.cpp
@@ -2,8 +2,7 @@
 #include<conio.h>
 void funcCharacterArray()
 {
-	printf("::::::::lets have some fun with Character Array:::::::::::\n"); i
-		printf("Let say i have comitted some error");
+	printf("::::::::lets have some fun with Character Array:::::::::::\n");i
 
 
 
